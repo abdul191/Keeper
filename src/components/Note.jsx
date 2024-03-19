e6cd3b6a708d8d1669cdccd07e78d507148a368c
Abdul Rehman <97659086+abdul191@users.noreply.github.com>
2024-03-20 02:46:18 +0500

    Keeper

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Hello</h1>
+      <p>hope everyThing will be okay </p>
+    </div>
+  );
+}
+
+export default Note;
