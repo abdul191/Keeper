aaddc730b2f23f65c09f9dac8288cd87609bcb35
Abdul Rehman <97659086+abdul191@users.noreply.github.com>
2024-03-27 03:11:23 +0500

    modified Note component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes"
+console.log(notes)
+
 function App() {
   return (
     <div>
       <Header />
       <Footer />
-      <Note />
+      {/* Function for the Note it will show all the notes in the Note Compnent */}
+      {notes.map((noteItem)=>
+        <Note
+        key= {noteItem.key}
+        title = {noteItem.title}
+        content = {noteItem.content}        
+        />
+        )}
     </div>
   );
 }
